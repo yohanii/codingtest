1387cf8254a3f66d05310f372b8cc400d0d06d55
Yohan Kim <yohan.kim@bagelcode.com>
2023-07-03 20:21:45 +0900

    solve 9184, 11053

--- java algorithm/src/Main.java
+++ java algorithm/src/Main.java
@@ -7,30 +7,44 @@ import java.util.stream.Stream;
 
 
 public class Main {
-
-    public void draw(int i, int j, int num) {
-        if((i/num)%3 == 1 && (j/num)%3 == 1) {
-            System.out.print(" ");
-        }else {
-            if(num/3 == 0) {
-                System.out.print("*");
-            }else{
-                draw(i, j, num/3);
-            }
-        }
-
-
-    }
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                draw(i, j, n);
-            }
-            System.out.println("");
+            st = new StringTokenizer(br.readLine());
+            Integer a = Integer.parseInt(st.nextToken());
+            Integer b = Integer.parseInt(st.nextToken());
+
+            map.put(a, b);
         }
+        Integer[] keyArr = map.keySet().toArray(new Integer[0]);
+        Integer[] dp = new Integer[k+1];
+        for(int i = 1; i < k+1; i++) {
+            if(dp[i] == null) {
+                if(map.get(i) ==null) {
+                    dp[i] = 0;
+                } else {
+                    dp[i] = map.get(i);
+                }
+            }
+            int j = 0;
+            while(i > j && j < keyArr.length) {
+                if(i - keyArr[j] > 0) {
+                    if(dp[keyArr[j]] + dp[i - keyArr[j]] > dp[i]) {
+                        dp[i] = dp[keyArr[j]] + dp[i - keyArr[j]];
+                    }
+                } else {
+                    break;
+                }
+                j++;
+            }
+        }
+        System.out.println(dp[k]);
     }
 
     public static void main(String[] args) throws Exception {
