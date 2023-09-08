98739dae437cb63bd3986e651b82a5b1024355b0
yohanii <dygks924@gmail.com>
2023-09-08 22:20:18 +0900

    solve 9375

--- java algorithm/src/백준_9375.java
+++ java algorithm/src/백준_9375.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 
-public class Main {
+public class 백준_9375 {
 
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
