d6e24a2a7f9421df32426006060ab67b6da2f95c
yohanii <dygks924@gmail.com>
2023-11-23 22:30:15 +0900

    solve 2023KAKAO two problems

--- java algorithm/src/programmers_2023KAKAO_표현가능한이진트리.java
+++ java algorithm/src/programmers_2023KAKAO_표현가능한이진트리.java
@@ -1,13 +1,51 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
+public class programmers_2023KAKAO_표현가능한이진트리 {
 
-public class Main {
+    private static boolean isSatisfied(String fullBinary) {
+        int length = fullBinary.length();
+//        System.out.println("length = " + length);
+        if (length == 1) {
+//            System.out.println("Main.isSatisfied : End");
+            return true;
+        }
+        if (length % 2 == 0) {
+//            System.out.println("Main.isSatisfied : EVEN");
+            return false;
+        }
+        int rootIndex = (length - 1) / 2;
+
+//        System.out.println("rootIndex = " + rootIndex);
+        if (fullBinary.substring(rootIndex, rootIndex + 1).equals("1")) {
+            return isSatisfied(fullBinary.substring(0, rootIndex)) && isSatisfied(fullBinary.substring(rootIndex + 1));
+        }
+        return fullBinary.equals("0".repeat(length));
+    }
+
+    private static int getFullLength(int binaryLength) {
+        int result = 1;
+
+        while (result < binaryLength) {
+            result = 2 * result + 1;
+        }
+        return result;
+    }
+
+    private static String numToBinary(long number) {
+        String result = "";
+        long remain = number;
+
+        while (remain != 0) {
+            long l = remain % 2;
+            result = l + result;
+            remain /= 2;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
 
     public void solution() throws Exception {
 
@@ -37,50 +75,5 @@ public class Main {
             return result;
         }
     }
-
-    private static boolean isSatisfied(String fullBinary) {
-        int length = fullBinary.length();
-//        System.out.println("length = " + length);
-        if (length == 1) {
-//            System.out.println("Main.isSatisfied : End");
-            return true;
-        }
-        if (length % 2 == 0) {
-//            System.out.println("Main.isSatisfied : EVEN");
-            return false;
-        }
-        int rootIndex = (length - 1) / 2;
-
-//        System.out.println("rootIndex = " + rootIndex);
-        if (fullBinary.substring(rootIndex, rootIndex + 1).equals("1")) {
-            return isSatisfied(fullBinary.substring(0, rootIndex)) && isSatisfied(fullBinary.substring(rootIndex + 1));
-        }
-        return fullBinary.equals("0".repeat(length));
-    }
-
-    private static int getFullLength(int binaryLength) {
-        int result = 1;
-
-        while(result < binaryLength) {
-            result = 2 * result + 1;
-        }
-        return result;
-    }
-
-    private static String numToBinary(long number) {
-        String result = "";
-        long remain = number;
-
-        while(remain != 0) {
-            long l = remain % 2;
-            result = l + result;
-            remain /= 2;
-        }
-        return result;
-    }
-
-
-    public static void main(String[] args) throws Exception {
-        new Main().solution();
-    }
 }
+
