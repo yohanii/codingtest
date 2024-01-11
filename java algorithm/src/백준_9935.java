8f70c5737f245535e8c5ba780c916349f57109d6
김요한 <kim-yohan@gim-yohan-ui-MacBookPro.local>
2024-01-11 18:20:20 +0900

    solve 9935

--- java algorithm/src/백준_9935.java
+++ java algorithm/src/백준_9935.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Main {
+public class 백준_9935 {
 
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
