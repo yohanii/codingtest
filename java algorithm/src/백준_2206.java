8af9500fcf0562aac4f6354188f206cc8ad597af
yohan <dygks924@gmail.com>
2024-01-21 16:48:05 +0900

    solve 2206

--- java algorithm/src/백준_2206.java
+++ java algorithm/src/백준_2206.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Main {
+public class 백준_2206 {
 
     static int[][] map;
     static int[][][] visited;
