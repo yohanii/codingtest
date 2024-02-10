d0b0ecfddb9be2c6a802d53746af5b77bd439d40
yohan <dygks924@gmail.com>
2024-02-10 19:50:29 +0900

    solve 2473

--- java algorithm/src/백준_2473.java
+++ java algorithm/src/백준_2473.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Main {
+public class 백준_2473 {
     static int n;
     static List<Long> liquids;
     static long minValue;
