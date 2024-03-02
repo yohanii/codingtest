2dde3f9d4989cdedee0fa485814f0c3c7ddc4948
yohan <dygks924@gmail.com>
2024-03-02 18:39:31 +0900

    solve algospot PICNIC

--- java algorithm/src/알고스팟_PICNIC.java
+++ java algorithm/src/알고스팟_PICNIC.java
@@ -3,11 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.*;
+import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Main {
+public class 알고스팟_PICNIC {
 
     static int count;
     static List<Integer>[] friend;
@@ -15,6 +15,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+
+        //개선점
+        //1. friend를 list 배열 말고, 2차원 배열에 해놓고, 모든 사람돌면서 friend 조건 확인으로 고칠 수 있음. m이 크면 이 방법이 나을지도.
+        //2. connect parameter로 left 남은 사람들 전달해주는데, 배열로 해서, 짝지어진 사람, 아닌 사람 구분해도 좋음.
+        //3. 친구 1명인 사람들 고르고, 고른 사람도 체크해서, 처음에 몇 명 지우고 시작하면 빠를 듯.
         int t = Integer.parseInt(br.readLine().trim());
 
         while (t-- > 0) {
@@ -72,3 +77,4 @@ public class Main {
         new Main().solution();
     }
 }
+
