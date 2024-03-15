75b0ddf97f819a1263b1d27f574b206d0815bb24
yohan <dygks924@gmail.com>
2024-03-15 18:09:49 +0900

    solve 10942

--- java algorithm/src/백준_10942.java
+++ java algorithm/src/백준_10942.java
@@ -7,7 +7,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Main {
+public class 백준_10942 {
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
